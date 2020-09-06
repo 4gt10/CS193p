d6cb1f142cf0f576a6c3a0ddfe98fd5532d6431a
Artur Chernov <forgot10soul@gmail.com>
2020-09-06 14:11:34 +0700

    Initial project structure

---
+++ Memorize/Memorize/Memorize/UI/ContentView/ContentViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ContentViewModel.swift
+//  Memorize
+//
+//  Created by Артур Чернов on 9/6/20.
+//  Copyright © 2020 Артур Чернов. All rights reserved.
+//
+
+import Foundation
+
+final class ContentViewModel {
+    
+}
